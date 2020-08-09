702855bc0d8c1df7b99d62d03a0dc71a229b82f3
Finn-Mal <68063595+Finn-Mal@users.noreply.github.com>
2020-08-09 11:30:47 +1000

    Pacman Maze level1 Update
    PacMan now has working animation

--- PacMan_2805ICT/Assets/Scripts/PacMan.cs
+++ PacMan_2805ICT/Assets/Scripts/PacMan.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player_Input : MonoBehaviour
+public class PacMan : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
