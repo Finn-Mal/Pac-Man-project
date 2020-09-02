4b909611ea3d46144a29a11d6d6a747d5a609d51
Finn-Mal <68063595+Finn-Mal@users.noreply.github.com>
2020-09-02 11:13:15 +1000

    Pellet Nodes - Vertex addition begin

--- PacMan_2805ICT/Assets/Scripts/Node.cs
+++ PacMan_2805ICT/Assets/Scripts/Node.cs
@@ -2,8 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Node Script from Chris Fletcher The Weekly Coder Youtube Tutorial
+
 public class Node : MonoBehaviour
 {
+
+    public Node[] Neighbours;
+    public Vector2[] valid_Direction;
+
     // Start is called before the first frame update
     void Start()
     {
