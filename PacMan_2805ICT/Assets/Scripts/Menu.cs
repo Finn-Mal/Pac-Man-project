5114b7cadfdcfdf9edd6c05a65788f798297ec0e
Finn-Mal <68063595+Finn-Mal@users.noreply.github.com>
2020-10-11 12:42:37 +1000

    Main Menu and Gamescore UI
    Score decrements 1 point per second while player has not collected all pellets

---
+++ PacMan_2805ICT/Assets/Scripts/Menu.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    private enum MenuChoice {PlayGame, Options, Exit } 
+    private MenuChoice ToLoad;
+
+    public Text PlayGame;
+    public Text Options;
+    public Text Quit;
+
+    public Image Selector;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.W)) && Selector.transform.position.y != PlayGame.transform.position.y)
+        {
+            Selector.transform.localPosition = new Vector3(Selector.transform.localPosition.x, Selector.transform.localPosition.y + 75f, Selector.transform.localPosition.z);
+        }
+        else if ((Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.S)) && Selector.transform.position.y != Quit.transform.position.y)
+        {
+            Selector.transform.localPosition = new Vector3(Selector.transform.localPosition.x, Selector.transform.localPosition.y  - 75f, Selector.transform.localPosition.z);
+        }
+
+
+        if(Input.GetKeyUp(KeyCode.Space) || Input.GetKeyUp(KeyCode.Return))
+        {
+            LoadMenuOption();
+        }
+
+        checkMenuposition();
+    }
+
+    void checkMenuposition()
+    {
+        if(Selector.transform.position.y == PlayGame.transform.position.y)
+        {
+            ToLoad = MenuChoice.PlayGame;
+        }
+        else if(Selector.transform.position.y == Options.transform.position.y)
+        {
+            ToLoad = MenuChoice.Options;
+        }
+        else if(Selector.transform.position.y == Options.transform.position.y)
+        {
+            ToLoad = MenuChoice.Exit;
+        }
+    }
+
+    void LoadMenuOption()
+    {
+
+        if(ToLoad == MenuChoice.PlayGame)
+        {
+            SceneManager.LoadScene("Level1");
+        }
+        else if(ToLoad == MenuChoice.Options)
+        {
+            //SceneManage.LoadScene("Options");
+        }
+        else if(ToLoad == MenuChoice.Exit)
+        {
+            Application.Quit();
+        }
+
+
+    }
+}
--- PacMan_2805ICT/Assets/Scripts/The_Board.cs
+++ PacMan_2805ICT/Assets/Scripts/The_Board.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class The_Board : MonoBehaviour
 {
@@ -9,20 +11,28 @@ public class The_Board : MonoBehaviour
     private static int boardWidth = 28;
     private static int boardHeight = 36; // 28 x 36 is the standard pacman board size
 
+    private float Timepassed = 0f;
+
+    
+    public Text HighScore;
+    public Image[] LivesRemaining = new Image[3];
+
     // an array to store the X, Y cooridinates of a game object currently in the scene (maze Pixels, Pellets, PacMan etc
     public GameObject[,] Board = new GameObject[boardWidth, boardHeight];
     private GameObject[] Ghosts;
     private GameObject[] All_Tiles;
 
-    private int GameScore = 0;
+    private int GameScore = 100000;
     private int pelletsRemain = 0;
 
 
     private GameObject ThePacman;
 
-    public Ghost localGhost;
+    public int PacmanLives = 2;
 
-    public int PacmanLives = 3;
+
+    public AudioClip backgroundNormal;
+    public AudioClip backGroundFrighten;
 
     // Start is called before the first frame update
     void Start()
@@ -39,6 +49,7 @@ public class The_Board : MonoBehaviour
 
         }
 
+
         ThePacman = GameObject.FindWithTag("Player");
     }
 
@@ -47,7 +58,11 @@ public class The_Board : MonoBehaviour
     {
         MonitorGameboard();
 
+        UpdateScore();
+
         GameStatus();
+
+        LivesDisplayed();
     }
 
     void MonitorGameboard()
@@ -71,26 +86,33 @@ public class The_Board : MonoBehaviour
                         foreach(GameObject ScaredGhost in Ghosts)
                         {
                             ScaredGhost.GetComponent<Ghost>().FrightenModeActivate();
-                        }
-                        
+                        }   
                     }
                     tile.GetComponent<SpriteRenderer>().enabled = false;
                     Tilecomponent.setEaten(true);
-                    GameScore += 10;
                     pelletsRemain += 1;
 
-
                 }
-
-                
+ 
             }
-
-
         }
     }
 
 
+    void UpdateScore()
+    {
+        Timepassed += Time.deltaTime;
 
+        if(Timepassed >= 1)
+        {
+            Timepassed = 0f;
+            GameScore -= 1;
+            Debug.Log(GameScore);
+        }
+
+        HighScore.text = GameScore.ToString();
+        
+    }
 
     GameObject CheckTile()
     {
@@ -109,14 +131,9 @@ public class The_Board : MonoBehaviour
 
     void GameStatus()
     {
-        if(pelletsRemain == 244)
+        if(pelletsRemain == 244 || PacmanLives == 0)
         {
-
-        }
-
-        if(PacmanLives == 0)
-        {
-            //Gameover and return to main menu
+            SceneManager.LoadScene("MainMenu");
         }
 
     }
@@ -124,6 +141,7 @@ public class The_Board : MonoBehaviour
     public void Respawn()
     {
         PacmanLives -= 1;
+        GameScore -= 5000;
         ThePacman.GetComponent<PacMan>().Respawn();
         foreach (GameObject ghost in Ghosts)
         {
@@ -133,8 +151,16 @@ public class The_Board : MonoBehaviour
         }
     }
 
-    void loadNextLevel()
+    void LivesDisplayed()
     {
-
+        for(int i = 0; i < PacmanLives - 1; i++)
+        {
+            LivesRemaining[i].enabled = false;
+        }
+        for (int i = 0; i < PacmanLives - 1; i++)
+        {
+            LivesRemaining[i].enabled = true;
+        }
     }
+
 }
